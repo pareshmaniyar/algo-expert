81168dc5dddc1141814434f7ad926b6e2762de49
Paresh Maniyar <pareshmaniyar1@gmail.com>
2021-12-08 12:11:59 +0530

    Add faster tree code

--- leetcode/trees/findTilt.js
+++ leetcode/trees/findTilt.js
@@ -31,5 +31,37 @@ function traverse(root, sum) {
 
 
 
+// FASTER CODE
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+ var findTilt = function(root) {
+    return traverse(root)[1];
+};
+
+function traverse(root) {
+    if(root === null) return [0, 0];
+    const left = traverse(root.left);
+    const right = traverse(root.right);
+    let diff = left[0] - right[0];
+    if(diff < 0) {
+        diff = diff * -1;
+    }
+    return [left[0] + right[0] + root.val, diff + left[1] + right[1]];
+}
+
+
+
+
+
 
 
