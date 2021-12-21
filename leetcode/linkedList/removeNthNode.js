70f394fff796011f8363f5d61044c00f3afbc789
Paresh Maniyar <pareshmaniyar1@gmail.com>
2021-12-21 13:40:23 +0530

    Add linkedlist problem

---
+++ leetcode/linkedList/removeNthNode.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+/*
+1 - 2 - 3 - 4 - 5
+    T   S       N
+c = 3, n = 2
+
+*/
+var removeNthFromEnd = function(head, n) {
+    let fast = head, slow = head
+    for (let i = 0; i < n; i++) fast = fast.next
+    if (!fast) return head.next
+    while (fast.next) fast = fast.next, slow = slow.next
+    slow.next = slow.next.next
+    return head
+};
+
